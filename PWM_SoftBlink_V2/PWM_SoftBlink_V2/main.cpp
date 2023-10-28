a2fc9b96b8df51441994e2ea4a2241ad7760810a
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2023-10-28 13:20:14 +0200

    Updated Fast PWM code (SoftBlinkV2)

--- PWM_SoftBlink_V2/PWM_SoftBlink_V2/main.cpp
+++ PWM_SoftBlink_V2/PWM_SoftBlink_V2/main.cpp
@@ -5,13 +5,13 @@
  * Author : Thomas
  */ 
 
-#define F_CPU 8000000	// 8 [MHz] from datasheet: ATMEGA168-20PU
+// CPU frequency is 1 [MHz], after prescaler of 8 with fuse bit.
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdio.h>	// Usikker med denne
-#include <cmath>
-#include <util/delay.h>
-#define __DELAY_BACKWARD_COMPATIBLE__ // Required for variable delay
+//#include <stdio.h>
+#include <math.h>	// instead of cmath, less work since std library missing
+//#include <util/delay.h>
+//#define __DELAY_BACKWARD_COMPATIBLE__ // Required for variable delay
 #define PI 3.14159265
 
 void init()
@@ -33,13 +33,13 @@ void init()
 	// Set prescaler:
 		// f_Clk = 16 [MHz], f_des = 250 [kHz]
 		// Ratio = prescaler = N = 16/0.25 = 64
-	TCCR1B |=  (1 << CS10) | (1 << CS11);
-	TCCR1B &= ~(1 << CS12);
+	TCCR1B |=  (1 << CS11);
+	TCCR1B &= ~(1 << CS12) & ~(1 << CS10);
 	
   // --------------------- PWM Configuration ---------------------
   
 	// Configure Output Compare Pin: PB1 = pin 15
-	DDRB |= (1 << DDD1);
+	DDRB |= (1 << DDB1);
 }
 
 int main(void)
@@ -53,16 +53,21 @@ int main(void)
 	const double fsRad = f*2.0*PI;		// Speed of pulse, [rad/sec]
 
 		// Set the Duty Cycle
-	double dutyCycle = 0.0;	// Default Duty Cycle
-	OCR1A = dutyCycle * 1023;	// [bit]
+	double dutyCycle = 0.5;	// Default Duty Cycle
+	OCR1A = 0x0;
 	
 // ------------- Main Variables -------------
 	
     while (true) 
     {
-		// Soft Blink sine
+			// Updates PWM (Duty Cycle)
+		if ((dutyCycle > 0.0) & (dutyCycle < 1.0))	// Safeguard for [%]
+		{ OCR1A = static_cast<uint16_t>(dutyCycle * 1023.0); }	// [bit]
+		
+		
+			// Soft Blink sine
 		t += 0.01;
-		dutyCycle = (1.0 - std::sin(fsRad*t))*0.5; // Sets duty cycle for PWM
+		//dutyCycle = (1.0 - sin(fsRad*t))*0.5; // Sets duty cycle for PWM
     }
 	return 0;
 }
