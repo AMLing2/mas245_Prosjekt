c860b74b2dcd0d568bb68a01e882599b3c627eee
creamytoad <mathias.lervik@hotmail.com>
2023-11-01 15:20:28 +0100

    added ny compilerings test kode

---
+++ helloworld2/HelloWorld2/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * helloWorld2.cpp
+ *
+ * Created: 17/10/2023 10:43:29
+ * Author : mathi
+ */ 
+ 
+#include <avr/io.h>
+
+ int main(void)
+ {
+    int a = 1;
+    int b = 1;
+    int c;
+    c = a + b;
+ }
