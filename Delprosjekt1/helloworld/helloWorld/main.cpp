2f508070223b606a01d6811f1af660af48b12143
creamytoad <mathias.lervik@hotmail.com>
2023-11-23 09:30:08 +0100

    updated polling code (helloWorld)

--- Delprosjekt1/helloworld/helloWorld/main.cpp
+++ Delprosjekt1/helloworld/helloWorld/main.cpp
@@ -17,10 +17,10 @@
 
 void init()
 {
-	DDRB |= (1 << DDB1); //set pin 7 as output in data-direction of port D
-	DDRD &= ~(1 << PIND6); //set pin 6 of port D as input
-	PORTB &= ~(1<<PB1); //set pin 7 low as initial value
-	PORTD |= (1<<PD6); //set pin 6 high to enable pull up-resistor to read state
+	DDRB |= (1 << DDB1); //set pin B1 as output in data-direction of port D
+	DDRD &= ~(1 << PIND6); //set pin D6 of port D as input
+	PORTB &= ~(1<<PB1); //set pin B1 low as initial value
+	PORTD |= (1<<PD6); //set pin D6 high to enable pull up-resistor to read state
 }
 
 void blink()
@@ -36,20 +36,15 @@ int main(void)
 	//initialize
 	init();
 	
-	bool a = true;
-	uint8_t pin6Value = 0;
-	
 	while (true) // run forever
 	{
-		pin6Value = (PIND & (1 << PD6)) >> PD6; // read pin 6 value, 0 if low, 1 if high
-		
-		if (pin6Value == 0) // if pin 6 of port D is low
+		if ((( ( PIND & (1 << PD6) ) >> PD6 ) == 0)) //enable only if button is low/pushed down
 		{
-			a = ~a; //turn on or off blinking
+			PORTB |= (1<<PB1); //high
 		}
-		if (a)
+		else 
 		{
-			blink();
+			PORTB &= ~(1<<PB1); //low
 		}
 		
 	}
